b94280c89a0d85141e971d5734c24e1feca70020
Jimmy Ho <pointyhe0@gmail.com>
2019-04-02 13:52:09 -0700

    Cleaned code a bit on filter

--- pcpp_Filter.py
+++ pcpp_Filter.py
@@ -1,25 +1,25 @@
 from PCPartPicker_API import pcpartpicker as pcpp
 
 
-def getCPU(compList, cpu_info):
+def getCPU(compList, cpuList):
     '''
     '''
-    cpu_para = [
+    CPU_PARAMETER = [
         'AMD Ryzen', 'Intel Core i3-8', 'Intel Core i5-8', 'Intel Core i7-8',
         'Intel Core i3-9', 'Intel Core i5-9', 'Intel Core i7-9700',
         'Intel Core i9-9900K'
     ]
-    cpu_ex = 'OEM'
-    cpu_dict = []
+    CPU_EXCLUDE = 'OEM'
+    cpu_Dict = []
 
-    for cpu in cpu_info:
-        for model in cpu_para:
+    for cpu in cpuList:
+        for model in CPU_PARAMETER:
             if model in cpu['name']:
-                if cpu_ex not in cpu['name']:
+                if CPU_EXCLUDE not in cpu['name']:
                     if cpu['price'] != '':
-                        cpu_dict.append(cpu)
+                        cpu_Dict.append(cpu)
 
-    for cpu in cpu_dict:
+    for cpu in cpu_Dict:
         if type(cpu['price']) == float:
             continue
         else:
@@ -28,13 +28,13 @@ def getCPU(compList, cpu_info):
     desktop_type = compList["name"]
     chosen_set = []
     if 'gen' in desktop_type:
-        for cpu in cpu_dict:
+        for cpu in cpu_Dict:
             if 'AMD' in cpu['name'] and 'G' in cpu['name']:
                 chosen_set.append(cpu)
             if 'Intel' in cpu['name'] and 'F' not in cpu['name']:
                 chosen_set.append(cpu)
     elif 'game' in desktop_type or 'ws' in desktop_type:
-        for cpu in cpu_dict:
+        for cpu in cpu_Dict:
             if 'AMD' in cpu['name'] and 'G' not in cpu['name']:
                 chosen_set.append(cpu)
             if 'Intel' in cpu['name']:
@@ -58,79 +58,82 @@ def getmobo(compList, mobo_info, chosen_cpu):
     '''
     '''
     # Special ASUS motherboard from 8th gen that support 9th gen intel CPU
-    ASUS_INTEL9 = ['STRIX Z370-E', 'STRIX Z370-F', 'STRIX Z370-H',
-                   'STRIX Z370-G', 'PRIME Z370-A', 'TUF Z370-PRO GAMING',
-                   'TUF Z370-PLUS GAMING', 'PRIME Z370-P', 'STRIX Z370-I',
-                   'ROG MAXIMUS X ', 'STRIX H370-F', 'STRIX H370-I',
-                   'TUF H370-PRO GAMING', 'PRIME H370-A', 'PRIME H370-PLUS',
-                   'PRIME H370M-PLUS', 'STRIX B360-H GAMING',
-                   'STRIX B360-F GAMING', 'STRIX B360-G GAMING',
-                   'STRIX B360-I GAMING', 'TUF B360-PRO GAMING',
-                   'TUF B360-PLUS GAMING', 'TUF B360M-E GAMING',
-                   'TUF B360M-PLUS GAMING', 'PRIME B360-PLUS', 'PRIME B360M-A',
-                   'PRIME B360M-C', 'PRIME B360M-D', 'PRIME B360M-K',
-                   'EX-B360M-V', 'TUF H310-PLUS GAMING', 'PRIME H310-PLUS',
-                   'TUF H310M-PLUS GAMING', 'PRIME H310M-A', 'PRIME H310M-C',
-                   'PRIME H310I-PLUS', 'PRIME H310M-E', 'PRIME H310M-K',
-                   'PRIME H310M-D', 'PRIME H310T']
+    ASUS_INTEL9 = [
+        'STRIX Z370-E', 'STRIX Z370-F', 'STRIX Z370-H', 'STRIX Z370-G',
+        'PRIME Z370-A', 'TUF Z370-PRO GAMING', 'TUF Z370-PLUS GAMING',
+        'PRIME Z370-P', 'STRIX Z370-I', 'ROG MAXIMUS X ', 'STRIX H370-F',
+        'STRIX H370-I', 'TUF H370-PRO GAMING', 'PRIME H370-A',
+        'PRIME H370-PLUS', 'PRIME H370M-PLUS', 'STRIX B360-H GAMING',
+        'STRIX B360-F GAMING', 'STRIX B360-G GAMING', 'STRIX B360-I GAMING',
+        'TUF B360-PRO GAMING', 'TUF B360-PLUS GAMING', 'TUF B360M-E GAMING',
+        'TUF B360M-PLUS GAMING', 'PRIME B360-PLUS', 'PRIME B360M-A',
+        'PRIME B360M-C', 'PRIME B360M-D', 'PRIME B360M-K', 'EX-B360M-V',
+        'TUF H310-PLUS GAMING', 'PRIME H310-PLUS', 'TUF H310M-PLUS GAMING',
+        'PRIME H310M-A', 'PRIME H310M-C', 'PRIME H310I-PLUS', 'PRIME H310M-E',
+        'PRIME H310M-K', 'PRIME H310M-D', 'PRIME H310T'
+    ]
 
-    GIGA_INTEL9 = ['Z370 AORUS', 'Z370 HD3', 'Z370M D3H', 'Z370M DS3H',
-                   'Z370N WIFI', 'Z370P D3', 'Z370 UD3H', 'Z370XP SLI',
-                   'H370 AORUS GAMING 3', 'H370M D3H', 'H370M DS3H',
-                   'H370M D3H', 'H370 HD3', 'H370N WIFI', 'B360 AORUS',
-                   'B360 HD3', 'B360M GAMING', 'B360 HD3P', 'B360M D2V',
-                   'B360M D3H', 'B360M D3P', 'B360M D3V', 'B360M DS3H',
-                   'B360M H', 'B360M HD3', 'B360N WIFI', 'H310M S2H',
-                   'H310M DS2', 'H310 D3', 'H310M A', 'H310M H', 'H310M S2',
-                   'H310N']
+    GIGA_INTEL9 = [
+        'Z370 AORUS', 'Z370 HD3', 'Z370M D3H', 'Z370M DS3H', 'Z370N WIFI',
+        'Z370P D3', 'Z370 UD3H', 'Z370XP SLI', 'H370 AORUS GAMING 3',
+        'H370M D3H', 'H370M DS3H', 'H370M D3H', 'H370 HD3', 'H370N WIFI',
+        'B360 AORUS', 'B360 HD3', 'B360M GAMING', 'B360 HD3P', 'B360M D2V',
+        'B360M D3H', 'B360M D3P', 'B360M D3V', 'B360M DS3H', 'B360M H',
+        'B360M HD3', 'B360N WIFI', 'H310M S2H', 'H310M DS2', 'H310 D3',
+        'H310M A', 'H310M H', 'H310M S2', 'H310N'
+    ]
     # Special Gigabyte motherboard from 8th gen that support 9th gen intel CPU
 
-    MSI_INTEL9 = ['MSI B360 GAMING', 'MSI B360-A PRO', 'MSI B360M BAZOOKA',
-                  'MSI B360I GAMING PRO AC', 'MSI B360M GAMING',
-                  'MSI B360M MORTAR', 'MSI B360M PRO-V', 'MSI H310-A PRO',
-                  'MSI H310I PRO', 'MSI H310M GAMING', 'MSI H310M PRO-',
-                  'MSI H370 GAMING', 'MSI H370M BAZOOKA', 'MSI Z370 GAMING',
-                  'MSI Z370 GODLIKE', 'MSI Z370 KRAIT', 'MSI Z370 PC PRO',
-                  'MSI Z370 SLI PLUS', 'MSI Z370 TOMAHAWK', 'MSI Z370-A PRO',
-                  'MSI Z370I GAMING PRO', 'MSI Z370M GAMING PRO',
-                  'MSI Z370M MORTAR']
+    MSI_INTEL9 = [
+        'MSI B360 GAMING', 'MSI B360-A PRO', 'MSI B360M BAZOOKA',
+        'MSI B360I GAMING PRO AC', 'MSI B360M GAMING', 'MSI B360M MORTAR',
+        'MSI B360M PRO-V', 'MSI H310-A PRO', 'MSI H310I PRO',
+        'MSI H310M GAMING', 'MSI H310M PRO-', 'MSI H370 GAMING',
+        'MSI H370M BAZOOKA', 'MSI Z370 GAMING', 'MSI Z370 GODLIKE',
+        'MSI Z370 KRAIT', 'MSI Z370 PC PRO', 'MSI Z370 SLI PLUS',
+        'MSI Z370 TOMAHAWK', 'MSI Z370-A PRO', 'MSI Z370I GAMING PRO',
+        'MSI Z370M GAMING PRO', 'MSI Z370M MORTAR'
+    ]
     # Special MSI motherboard from 8th gen that support 9th gen intel CPU
 
-    ASROCK_INTEL9 = ['ASRock Z370M-ITX/ac', 'ASRock Z370M Pro4',
-                     'ASRock Z370 Taichi', 'ASRock Z370 Pro4',
-                     'ASRock Z370 Killer SLI', 'ASRock Z370 Extreme4',
-                     'ASRock H370M-ITX/ac', 'ASRock H370M Pro4',
-                     'ASRock H370 Pro4', 'ASRock H310M-ITX/ac',
-                     'ASRock H310M-HDV', 'ASRock H310M-G/M.2',
-                     'ASRock H310M-DGS', 'ASRock Fatal1ty Z370',
-                     'ASRock Fatal1ty H370 Performance',
-                     'ASRock Fatal1ty B360 Gaming K4', 'ASRock B360M-ITX/ac',
-                     'ASRock B360M-HDV', 'ASRock B360M Pro4',
-                     'ASRock B360M Performance', 'ASRock B360 Pro4']
+    ASROCK_INTEL9 = [
+        'ASRock Z370M-ITX/ac', 'ASRock Z370M Pro4', 'ASRock Z370 Taichi',
+        'ASRock Z370 Pro4', 'ASRock Z370 Killer SLI', 'ASRock Z370 Extreme4',
+        'ASRock H370M-ITX/ac', 'ASRock H370M Pro4', 'ASRock H370 Pro4',
+        'ASRock H310M-ITX/ac', 'ASRock H310M-HDV', 'ASRock H310M-G/M.2',
+        'ASRock H310M-DGS', 'ASRock Fatal1ty Z370',
+        'ASRock Fatal1ty H370 Performance', 'ASRock Fatal1ty B360 Gaming K4',
+        'ASRock B360M-ITX/ac', 'ASRock B360M-HDV', 'ASRock B360M Pro4',
+        'ASRock B360M Performance', 'ASRock B360 Pro4'
+    ]
     # Special ASROCK motherboard from 8th gen that support 9th gen intel CPU
 
     INTEL_9_BASE = ['Z390', 'Asus ROG MAXIMUS XI ']
     # Basic motherboard chipset that support Intel 9th gen CPU
 
-    intel_9_para = INTEL_9_BASE + ASUS_INTEL9 + \
+    INTEL_9_PARA = INTEL_9_BASE + ASUS_INTEL9 + \
         GIGA_INTEL9 + MSI_INTEL9 + ASROCK_INTEL9
     # Parameter for all motherboard that support intel 9th gen CPU
 
-    intel_8_para = ['Z370', 'H310', 'H370', 'Z390', 'B360',
-                    'Asus ROG MAXIMUS X ', 'Asus ROG MAXIMUS XI ']
+    INTEL_8_PARA = [
+        'Z370', 'H310', 'H370', 'Z390', 'B360', 'Asus ROG MAXIMUS X ',
+        'Asus ROG MAXIMUS XI '
+    ]
     # Parameter for all motherboard that support intel 8th gen CPU
 
-    amd_para = ['A320', 'B350', 'B450', 'X370', 'X470', 'Asus CROSSHAIR VI ']
+    AMD_PARA = ['A320', 'B350', 'B450', 'X370', 'X470', 'Asus CROSSHAIR VI ']
     # Parameter for all motherboard that support AMD Ryzen CPU
 
-    intel_8_cpu = ['Intel Core i3-8', 'Intel Core i5-8', 'Intel Core i7-8']
-    intel_9_cpu = ['Intel Core i3-9', 'Intel Core i5-9',
-                   'Intel Core i7-9700', 'Intel Core i9-9900K']
-    amd_cpu = ['AMD Ryzen']
+    INTEL_8_CPU = ['Intel Core i3-8', 'Intel Core i5-8', 'Intel Core i7-8']
+    INTEL_9_CPU = [
+        'Intel Core i3-9', 'Intel Core i5-9', 'Intel Core i7-9700',
+        'Intel Core i9-9900K'
+    ]
+    AMD_CPU = ['AMD Ryzen']
     # list of names to identify CPU brand and series
 
-    cpu_model = [intel_8_cpu, intel_9_cpu, amd_cpu]
-    motherboard_for_cpu = [intel_8_para, intel_9_para, amd_para]
+    cpu_model = [INTEL_8_CPU, INTEL_9_CPU, AMD_CPU]
+    motherboard_for_cpu = [INTEL_8_PARA, INTEL_9_PARA, AMD_PARA]
 
     mobo_dict = []
     for model in cpu_model:
@@ -153,7 +156,9 @@ def getmobo(compList, mobo_info, chosen_cpu):
     mobo_dict_budget = []
     for motherboard in mobo_dict:
         if motherboard['price'] < compList['motherboard']:
-            mobo_dict_budget.append(motherboard)  # alternative list of motherboard, filtered by price
+            mobo_dict_budget.append(
+                motherboard
+            )  # alternative list of motherboard, filtered by price
 
     chosen_mobo_set = []
     if 'Intel' in chosen_cpu['name']:
@@ -189,7 +194,9 @@ def getmobo(compList, mobo_info, chosen_cpu):
 
     if mobo_set == []:
         for motherboard in mobo_dict_budget:
-            mobo_set.append(motherboard)  # use alternative list if the optimal list is empty
+            mobo_set.append(
+                motherboard
+            )  # use alternative list if the optimal list is empty
 
     mobo_p = []
     for motherboard in mobo_set:
@@ -313,7 +320,8 @@ def getstor(compList, stor_info):
             drives['price/gb'] = float(drives['price/gb'].strip('$'))
         if drives['capacity'] != '':
             if 'TB' in drives['capacity']:
-                drives['capacity'] = float(drives['capacity'].strip(' TB'))*1024
+                drives['capacity'] = float(
+                    drives['capacity'].strip(' TB')) * 1024
             elif 'GB' in drives['capacity']:
                 drives['capacity'] = float(drives['capacity'].strip(' GB'))
 
@@ -326,7 +334,8 @@ def getstor(compList, stor_info):
 
     ssd_budget_list = []
     for drives in ssd_dict:
-        if 'M.2-2280' == drives['form'] and drives['price'] <= compList['storage']:
+        if 'M.2-2280' == drives[
+                'form'] and drives['price'] <= compList['storage']:
             ssd_budget_list.append(drives)
     # filter specific form factor and SSD within price range
 
@@ -355,7 +364,7 @@ def getstor(compList, stor_info):
 
     ssd_value_list = []
     for index, pgb in enumerate(ssd_pgb_list, 0):
-        if pgb < min(ssd_pgb_list)*1.2:
+        if pgb < min(ssd_pgb_list) * 1.2:
             ssd_value_list.append(ssd_size_list[index])
     # filter SSD to the lowest price/gb and within 20% more
 
@@ -383,7 +392,8 @@ def getstor(compList, stor_info):
             if brand in drives['name']:
                 for model in hdd_model:
                     if model in drives['series']:
-                        if drives['price'] <= hdd_budget and drives['form'] == '3.5"':
+                        if drives['price'] <= hdd_budget and drives[
+                                'form'] == '3.5"':
                             hdd_budget_list.append(drives)
     # filter specific form factor and models of HDD within price range
 
@@ -416,7 +426,8 @@ def getstor(compList, stor_info):
         if price == min(hdd_price_list):
             hdd_min_price.append(hdd_value_list[index])
 
-    if len(hdd_min_price) > 0 and hdd_min_price[0]['capacity'] > 900 and 'ws' in compList['name']:
+    if len(hdd_min_price) > 0 and hdd_min_price[0][
+            'capacity'] > 900 and 'ws' in compList['name']:
         select_hdd = hdd_min_price[0]
     else:
         select_hdd = 'NO HDD'
